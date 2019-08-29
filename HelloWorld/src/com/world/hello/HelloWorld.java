74535fae8a3d00c05bedbc27ee44f798bb15236e
ingquimbay <quimbayc@javeriana.edu.co>
2019-08-29 18:32:43 -0500

    An initial hello world in java.

---
+++ HelloWorld/src/com/world/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.world.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!\n");
+		System.out.println("hh          ll ll                                 ll    dd  !! ");
+		System.out.println("hh          ll ll                                 ll    dd  !! ");
+		System.out.println("hh          ll ll                                 ll    dd  !! ");
+		System.out.println("hhhhh  eee  ll ll  ooo     w        w  ooo  rr rr ll   ddd  !! ");
+		System.out.println("hh hh ee ee ll ll oo oo     w   w  w  oo oo rrr   ll dd dd  !! ");
+		System.out.println("hh hh eee   ll ll oo oo      w w w w  oo oo rr    ll dd dd  .. ");
+		System.out.println("hh hh  eeee ll ll  ooo        w   w    ooo  rr    ll  dddd  .. ");
+	}
+
+}
